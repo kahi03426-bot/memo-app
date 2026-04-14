d7ad56860a0e96b3fb436444ac50115c0f4575a3
Rina Watanabe <kahi03426@gmail.com>
2026-04-14 18:18:20 +0900

    ヘッダーの追加

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
     }
 }
--- laravel/resources/js/App.vue
+++ laravel/resources/js/App.vue
@@ -1,3 +1,8 @@
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
+
 <template>
-  <router-view />
+  <Header />
+<!--  <router-view />-->
 </template>
